398f757c1ec8fd6c41bebf925b1a9549aa8a9617
Michiel Bakker <michiel.bakker@infostrada.com>
2015-10-14 00:20:40 +0200

    Complete rewrite.

--- namespacejs.js
+++ namespacejs.js
@@ -31,59 +31,141 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-(function(window, undefined){
+var Namespace = (function(window){
 
-"use strict";
+    "use strict";
 
-function Namespace()
-{}
+    /**
+     * Regex for testing a namespace string.
+     *
+     * @private
+     *
+     * @type {RegExp}
+     */
+    var _namespacePattern = /^[a-z](?:\w*)?(\.[a-z](?:\w*)?)*$/i;
 
-Namespace.VALIDATOR = /^[a-z](?:\w*)?(\.[a-z](?:\w*)?)*$/i;
-Namespace.RESERVED_WORDS = 'instanceof this window function undefined true false null prototype constructor';
-
-Namespace.create = function(namespace, root)
-{
-    var parts, n, name, tree,
-        i = 0;
-
-    if (!Namespace.VALIDATOR.test(namespace)) {
-        throw new Error('Invalid namespace.');
+    /**
+     * Test if a given string can be converted to a namespace.
+     *
+     * @private
+     *
+     * @param  {string}  namespaceString The namespace to check.
+     * @return {boolean}                 True if the namespace string is valid,
+     *                                   false otherwise.
+     */
+    function _isValidNamespaceString(namespaceString)
+    {
+        return _namespacePattern.test(namespaceString);
     }
 
-    if (root === undefined) {
-        root = window;
+    /**
+     * Tests whether a variable contains a simple object.
+     *
+     * @private
+     *
+     * @param  {Object}   object A value to test.
+     * @return {boolean}         Returns if the value given is a simple object or not.
+     */
+    function _isSimpleObject(object)
+    {
+        return object instanceof Object && object.constructor === Object;
     }
 
-    parts = namespace.split('.');
-    for (n = parts.length; i < n; i++) {
-        name = parts[i];
+    /**
+     * Creates a new namespace.
+     *
+     * @private
+     *
+     * @param  {string} namespaceString The new namespace to create.
+     * @param  {Object} object          The new namespace to create.
+     */
+    function _createNamespace(namespaceString, object)
+    {
+        var parts, n, name,
+            i = 0,
+            target = window;
 
-        if (Namespace.isReservedWord(name)) {
-            throw new Error('Cannot create namespace: "' + name + '" is a reserved word.');
+        if (!_isValidNamespaceString(namespaceString)) {
+            throw new Error('Invalid namespace.');
         }
 
-        if (!(name in root)) {
-            root[name] = {};
+        parts = namespaceString.split('.');
+        for (n = parts.length; i < n; i++) {
+            name = parts[i];
 
-        } else if (!Namespace.isValid(root[name])) {
-            throw new Error('Can only create namespaces on simple objects.');
+            if (!(name in target)) {
+                target[name] = {};
 
+            } else if (!_isSimpleObject(target[name])) {
+                throw new Error('Can only create namespaces on simple objects.');
+
+            }
+
+            target = target[name];
         }
 
-        root = root[name];
+        // If object specified, register its contents to the namespace.
+        if (object) {
+            for (name in object) {
+                target[name] = object;
+            }
+        }
     }
-};
 
-Namespace.isReservedWord = function(word)
-{
-    return Namespace.RESERVED_WORDS.split(' ').indexOf(word) !== -1;
-};
+    /**
+     * Returns a reference to the namespace.
+     *
+     * @private
+     *
+     * @param  {string} namespaceString The namespace.
+     * @return {Object|undefined}       A reference to the namespace, or undefined if the
+     *                                  namespace doesn't exist.
+     */
+    function _getReference(namespaceString)
+    {
+        var namespace = window,
+            parts = namespaceString.split('.'),
+            i = 0,
+            n = parts.length;
 
-Namespace.isValid = function(namespace)
-{
-    return namespace instanceof Object && namespace.constructor === Object;
-};
+        for (; i < n; i++) {
+            if (!(parts[i] in namespace)) {
+                return undefined;
+            }
 
-window.Namespace = Namespace;
+            namespace = namespace[parts[i]];
+        }
+
+        return namespace;
+    }
+
+    /**
+     * Create a namespace.
+     *
+     * @public
+     *
+     * @param {string} namespaceString The namespace to create.
+     * @param {Object} object          If specified, register this object to the namespace
+     *                                 (key/value).
+     */
+    function Namespace(namespaceString, object)
+    {
+        _createNamespace(namespaceString, object);
+    }
+
+    /**
+     * Tells whether a namespace exists or not.
+     *
+     * @public
+     *
+     * @param  {string}  namespaceString The namespace to check.
+     * @return {boolean}                 True if the namespace exists, false otherwise.
+     */
+    Namespace.exists = function(namespaceString)
+    {
+        return _getReference(namespaceString) !== undefined;
+    };
+
+    return Namespace;
 
 }(window));
